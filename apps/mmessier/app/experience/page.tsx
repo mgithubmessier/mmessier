471eeed8dac486a13991ba40c5d40a64d99fee43
Matthew Messier <messier_matthew@yahoo.com>
2023-10-28 10:42:29 -0400

    hmmm, I don't know if I like this approach to requests -- I feel that debugging this would be a nightmare in production, having to look at server logs and narrow down to the session that I am testing from

--- apps/mmessier/app/experience/page.tsx
+++ apps/mmessier/app/experience/page.tsx
@@ -1,14 +1,19 @@
 import { Metadata } from 'next';
 
+import { getExperience } from '../../lib/getExperience';
+
 export const metadata: Metadata = {
   title: 'Experience',
   description: 'A summary of my professional experience',
 };
 
-export const Experience = () => {
+export const Experience = async () => {
+  const experience: Promise<any[]> = await getExperience();
+
   return (
     <div>
       <h1>Experience</h1>
+      <pre>{JSON.stringify(experience)}</pre>
     </div>
   );
 };
---
+++ apps/mmessier/lib/getExperience.ts
@@ -0,0 +1,7 @@
+export const getExperience = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  if (!response.ok) {
+    throw new Error('encountered error while retrieved experiences');
+  }
+  return response.json();
+};
