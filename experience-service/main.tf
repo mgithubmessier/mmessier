f1bb57aa78aa7e4d95f1b588e7886bb34df181c2
Matthew Messier <messier_matthew@yahoo.com>
2023-12-18 00:21:16 -0500

    I am so close here, rollup just needs to get the dependencies and we need to target the right module with the main tf

--- experience-service/main.tf
+++ experience-service/main.tf
@@ -1,3 +1,4 @@
+# based off of https://developer.hashicorp.com/terraform/tutorials/aws/lambda-api-gateway
 # sets up terraform dependencies
 terraform {
   required_providers {
@@ -26,8 +27,8 @@ resource "aws_s3_bucket" "lambda_bucket" {
 data "archive_file" "experience_service" {
   type = "zip"
 
-  source_dir  = "${path.module}/../dist/experience-service/src"
-  output_path = "${path.module}/../dist/experience-service/experience-service.zip"
+  source_dir  = "${path.module}/../dist/experience-service"
+  output_path = "${path.module}/../dist/experience-service.zip"
 }
 
 resource "aws_s3_object" "experience_service" {
@@ -47,7 +48,7 @@ resource "aws_lambda_function" "experience_service" {
   s3_key    = aws_s3_object.experience_service.key
 
   runtime = "nodejs20.x"
-  handler = "index.handler"
+  handler = "index.cjs.handler"
 
   source_code_hash = data.archive_file.experience_service.output_base64sha256
 
@@ -80,4 +81,73 @@ resource "aws_iam_role" "lambda_exec" {
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# sets up API Gateway 
+
+resource "aws_apigatewayv2_api" "lambda" {
+  name          = "experience_service_api_gateway"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "lambda" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  name        = "experience_service_stage"
+  auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gw.arn
+
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+      }
+    )
+  }
+}
+
+resource "aws_apigatewayv2_integration" "experience_service" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  integration_uri    = aws_lambda_function.experience_service.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "experience_service_GET" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  route_key = "GET /experiences"
+  target    = "integrations/${aws_apigatewayv2_integration.experience_service.id}"
+}
+
+resource "aws_apigatewayv2_route" "experience_service_GETONE" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  route_key = "GET /experiences/{experienceID}"
+  target    = "integrations/${aws_apigatewayv2_integration.experience_service.id}"
+}
+
+resource "aws_cloudwatch_log_group" "api_gw" {
+  name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
+
+  retention_in_days = 1
+}
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.experience_service.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
 }
